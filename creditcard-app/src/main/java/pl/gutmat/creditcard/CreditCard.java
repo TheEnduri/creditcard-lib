849dc3e5785e1d575994af36d383fc31786bd4ec
Gut Mateusz <gutmateusz96@interia.pl>
2018-11-08 10:34:19 +0000

    Update methods : withdraw / repaydebt

--- creditcard-app/src/main/java/pl/gutmat/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/gutmat/creditcard/CreditCard.java
@@ -3,13 +3,15 @@ package pl.gutmat.creditcard;
 class CreditCard {
     
     private boolean blockade = false;
+    private double limit;
+    private double balance;
     
     public void assignLimit(double money) {
-        
+        this.limit = money;
     }
     
     public double getLimit() {
-        return 2000;
+        return limit;
     }
     
     public void block() {
@@ -19,5 +21,16 @@ class CreditCard {
     public boolean isBlocked() {
         
         return this.blockade;
+    }
+    
+    public void withdraw(double amount){
+        balance = limit - amount;
+        
+        if(amount > limit){
+            block();
+        }
+        
+        
+        
     }
 }
