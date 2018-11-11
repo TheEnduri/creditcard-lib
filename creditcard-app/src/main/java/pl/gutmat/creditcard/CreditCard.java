ffb22a6abfda670748ae31f99781a2ea95001f25
user <email>
2018-11-11 12:38:34 +0000

    added withdraw

--- creditcard-app/src/main/java/pl/gutmat/creditcard/CreditCard.java
+++ creditcard-app/src/main/java/pl/gutmat/creditcard/CreditCard.java
@@ -1,25 +1,50 @@
 package pl.gutmat.creditcard;
 
 class CreditCard {
-    
+
     private boolean blockade = false;
-    private double limit;
-    private double balance;
+    private BigDecimal limit;
+    private BigDecimal balance;
     private String cardNumber;
-    
+
     public CreditCard(){
         this.cardNumber = "random_number";
     }
-    
+
     public CreditCard(String cardNumber){
         this.cardNumber = cardNumber;
     }
-    
+
     public String getNumber(){
         return cardNumber;
     }
-    
-    
+
+    public void withdraw(BigDecimal money) {
+        if (isWithdrawOverTheLimit(money))
+            throw new NotEnoughMoneyException();
+
+        if (isNotEnoughMoney(money))
+            throw new NotEnoughMoneyException();
+
+        if (isBlocked())
+            throw new TransactionOnBlockedCardException();
+
+        balance = balance.subtract(money);
+    }
+    public void block() {
+        this.blocked = true;
+    }
+
+    public boolean isBlocked() {
+        return this.blocked;
+    }
+    private boolean isNotEnoughMoney(BigDecimal money) {
+        return money.compareTo(balance) > 0;
+    }
+
+    private boolean isWithdrawOverTheLimit(BigDecimal money) {
+        return money.compareTo(limit) > 0;
+    }
     public void assignLimit(double money) {
         this.limit = money;
     }
@@ -32,5 +57,5 @@ class CreditCard {
         this.blockade = true;
     }
 
-        
+
 }
